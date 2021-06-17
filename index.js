70988ebc39415b16c89309d4c3231d2c6d91f8c7
flofloflofloflofloflo <floran.houdart@gmail.com>
2021-06-17 15:54:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require("dotenv").config();
+
+// Notre préfixe de commande
+const prefixCmd = '!';
+
+client.on("ready", () => {
+    console.log("I'm ready !");
+});
+
+client.on("message", msg => {
+
+    // Si le message n'est pas préfixé ou qu'il vient d'un autre bot, nous l'ignorons
+    if(!msg.content.startsWith(prefixCmd) || msg.author.bot) return
+
+    // Si nous arrivons jusque ici, alors c'est une commande
+
+    // Nous convertissons la commande sous forme de tableau en prenant soin de retirer le préfixe
+    const args = msg.content.slice(prefixCmd.length).trim().split(/ +/);
+    // Extraction du premier élément de 'args', ce qui correspond à la commande
+    const command = args.shift().toLowerCase();
+
+    // À ce stade, args est un tableau ne contenant que les arguments étant donné que la commande a été extraite de celui-ci
+
+    // On se sert maintenant de la varibale 'command' pour le test
+    if (command === "ping") {
+        msg.reply("pong");
+    }
+
+});
+
+client.login('ODU0NzE1NzY3OTY5OTM5NDk2.YMn-Fg.nyoLSwUG7--_v4E4_adDW-Fcpvs');
